8909e3f4be53cc19c5c68bb7263259656c5fbcb5
TalonRoberts <149705226+TalonRoberts@users.noreply.github.com>
2024-11-03 13:24:02 -0700

    Added Words_and_Count function
    - Added Words_and_Count function which takes in a string of any length and returns a list of every word with the number of times it appeared
    - Code at bottom has an example of how to use this function, though I may add a user input section in the future

--- Commit_Testing.py
+++ Commit_Testing.py
@@ -1,2 +1,23 @@
 # This is a basic file I will be utilizing to train and test my git skills
 # Python File
+def Top10ContentWords(s):
+    words_and_count = {}  # Initializes an empty dictionary
+    delims = [ ' ', ',', '!', '.', '?', '\n', '\r' ]      # All possible delimiters necessary to split words
+    s = s.lower()    # Makes entire string lowercase so things like "word" and "Word" will be considered the same
+    for delim in delims:
+        s = s.replace(delim, ' ')    # Split string into words based on delimiter
+    
+    wordsInString = s.split()   # Split by whitespace
+
+    for word in wordsInString:  # iterates through each word in inputted string
+        if word in words_and_count:  # Checks if words_and_count contains current word
+            words_and_count[word] += 1    # Adds word to wordCounts dictionary and increments its count by 1
+        else:
+            words_and_count[word] = 1  # First instance of word so adds word to wordCounts dictionary and sets count to 1
+    
+    return words_and_count
+
+# Usage
+s = "Hi my name is Talon Roberts, how are we doing on this fine evening. What is your favorite flavor of Starburst?"
+word_counts = Top10ContentWords(s)
+print(word_counts)
